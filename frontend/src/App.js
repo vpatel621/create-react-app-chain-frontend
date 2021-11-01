d449c61bb2cf262ec94e375eaa1a15648bd419a5
Viral Patel <vpatel621@gmail.com>
2021-11-01 17:56:24 -0400

    Feature:Table Data for currency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Table } from 'react-bootstrap';
 import { Icon, InlineIcon } from '@iconify/react';
 import ethIcon from '@iconify/icons-cryptocurrency/eth';
 import btc from '../node_modules/cryptocurrency-icons/32/color/btc.png';
@@ -8,36 +9,34 @@ export default function App() {
   const ws = useRef(null);
   const [buy, setBuy] = useState([]);
   const [filterC, setFilter] = useState([]);
+  const [status, setStatus] = useState(false);
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:3030');
     ws.current.onopen = () => console.log('ws opened');
     ws.current.onclose = () => console.log('ws closed');
 
-    const wsCurrent = ws.current;
-
     return () => {
-      wsCurrent.close();
+      ws.current.close();
     };
   }, []);
 
   useEffect(() => {
     if (!ws.current) return;
-
     ws.current.onmessage = (res) => {
       const message = JSON.parse(res.data);
-      setBuy(() => message.buyer);
+      setBuy(message.buyer);
     };
   }, [buy]);
 
   function filterMethod(type) {
+    setStatus(true);
     let filteredCryptos = buy;
 
     const res = filteredCryptos.filter((item) => {
       return item.name === type;
     });
     setFilter(res);
-    console.log('post', filterC);
   }
 
   return (
@@ -48,23 +47,72 @@ export default function App() {
         <span> </span>
         <img src={eth} onClick={() => filterMethod('ETH')} />
       </div>
-      {filterC.map((ele) => {
-        {
-        }
-        return (
-          <div>
-            {ele.name === 'ETH' ? (
-              <img
-                src='https://cryptologos.cc/logos/ethereum-eth-logo.png'
-                width='20'
-                height='30'
-                style={{ display: 'vertical-align:top' }}
-              />
-            ) : null}
-            {ele.name}: {ele.price}
-          </div>
-        );
-      })}
+      {status ? (
+        <div>
+          <Table variant='dark'>
+            <caption> Buy Data </caption>
+
+            <thead>
+              <tr>
+                <th> </th>
+                <th>Exchange</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterC.map((item) => {
+                return (
+                  <tr>
+                    <td>
+                      <img
+                        alt=''
+                        src='https://cryptologos.cc/logos/ethereum-eth-logo.png'
+                        width='20'
+                        height='30'
+                        style={{ display: 'vertical-align:top' }}
+                      />
+                    </td>
+                    <td>{item.source}</td>
+                    <td>{item.price}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <span>
+            <Table variant='dark'>
+              <caption> Sell Data </caption>
+
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Exchange</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filterC.map((item) => {
+                  return (
+                    <tr>
+                      <td>
+                        <img
+                          alt=''
+                          src='https://cryptologos.cc/logos/ethereum-eth-logo.png'
+                          width='20'
+                          height='30'
+                          style={{ display: 'vertical-align:top' }}
+                        />
+                      </td>
+                      <td>{item.source}</td>
+                      <td>{item.price}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 }
