ebdbf942d0fb569aeba95a9550c864311e7a9ac5
Viral Patel <vpatel621@gmail.com>
2021-11-01 15:27:41 -0400

    Feature: Filter between Ethereum and Bitcoin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Icon, InlineIcon } from '@iconify/react';
+import ethIcon from '@iconify/icons-cryptocurrency/eth';
+import btc from '../node_modules/cryptocurrency-icons/32/color/btc.png';
+import eth from '../node_modules/cryptocurrency-icons/32/color/eth.png';
 
 export default function App() {
   const ws = useRef(null);
   const [buy, setBuy] = useState([]);
-  const [sell, setSell] = useState([]);
+  const [filterC, setFilter] = useState([]);
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:3030');
@@ -22,36 +26,41 @@ export default function App() {
 
     ws.current.onmessage = (res) => {
       const message = JSON.parse(res.data);
-      console.log('presort', message.buyer);
-      message.buyer.sort((a, b) => a.price - b.price);
-      console.log('postsort', message.buyer);
       setBuy(() => message.buyer);
-      setSell(() => message.seller);
-
-      console.log('state', sell);
     };
-  }, [buy, sell]);
+  }, [buy]);
+
+  function filterMethod(type) {
+    let filteredCryptos = buy;
+
+    const res = filteredCryptos.filter((item) => {
+      return item.name === type;
+    });
+    setFilter(res);
+    console.log('post', filterC);
+  }
 
   return (
     <div className='container'>
-      HI Selling:
-      {sell.map((ele) => {
+      <h2>Cryptocurrencys</h2>
+      <div className='row-of-crypto'>
+        <img src={btc} onClick={() => filterMethod('BTC')} />
+        <span> </span>
+        <img src={eth} onClick={() => filterMethod('ETH')} />
+      </div>
+      {filterC.map((ele) => {
         {
-          console.log('outside return', ele);
         }
         return (
           <div>
-            {ele.name}: {ele.price}
-          </div>
-        );
-      })}
-      HI Buying:
-      {buy.map((ele) => {
-        {
-          console.log('outside return', ele);
-        }
-        return (
-          <div>
+            {ele.name === 'ETH' ? (
+              <img
+                src='https://cryptologos.cc/logos/ethereum-eth-logo.png'
+                width='20'
+                height='30'
+                style={{ display: 'vertical-align:top' }}
+              />
+            ) : null}
             {ele.name}: {ele.price}
           </div>
         );
