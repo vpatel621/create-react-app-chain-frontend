1f060f240840de94ed86e43fa5fb1dcab9b2da44
Viral Patel <75807168+vpatel621@users.noreply.github.com>
2021-11-06 18:45:49 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ export default function App() {
   const [coinSelect, setcoinSelect] = useState(false);
   const [isLoading, setLoading] = useState(true);
 
-  const url = 'http://localhost:3030/';
-  // process.env.REACT_APP_URL ||
+  const url = process.env.REACT_APP_URL || 'http://localhost:3030/';
+
   useEffect(() => {
     setTimeout(function () {
       setLoading(false);
